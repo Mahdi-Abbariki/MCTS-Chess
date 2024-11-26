1b779ea3b2dbc60718fbf6053798eb54f45ac1b0
Mahdi Abbariki <mahdi.abbariki0@gmail.com>
2024-11-26 20:46:10 +0330

    fix

--- .uploadToServer.sh
+++
@@ -1,7 +0,0 @@
-echo ".gitignore\n*/.gitignore\n*/.gitkeep\n.git-ftp-ignore\n.git-ftp-include\n.gitlab-ci.yml" > .git-ftp-ignore
-
-git config git-ftp.url "ftp://ftp.mahdiab.ir:21/chess.mahdiab.ir"
-git config git-ftp.user "mahdi@mahdiab.ir"
-git config git-ftp.password "Cwe3802689*."
-
-git ftp push
